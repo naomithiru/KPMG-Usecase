8dd6b24a4a30e0eecb5e22233a4ae361557030ca
Davy Nimbona <davynimbona@gmail.com>
2021-03-04 22:19:56 +0100

    Streamlit app

--- stream.py
+++ stream.py
@@ -3,6 +3,25 @@ import pandas as pd
 import datetime
 import pickle
 
+category_dict = {
+    "COMMISSION PARITAIRE DES CONSTRUCTIONS METALLIQUE, MECANIQUE ET ELECTRIQUE": [111, []],
+    "COMMISSION PARITAIRE DE L'INDUSTRIE CHIMIQUE": [116, []],
+    "COMMISSION PARITAIRE DE L'INDUSTRIE ALIMENTAIRE": [118, []],
+    "COMMISSION PARITAIRE POUR LE NETTOYAGE": [121, []],
+    "COMMISSION PARITAIRE DE LA CONSTRUCTION": [124, []],
+    "COMMISSION PARITAIRE DU TRANSPORT ET DE LA LOGISTIQUE": [140,
+    ["SOUS-COMMISSION PARITAIRE POUR LES AUTOBUS ET AUTOCARS", "SOUS-COMMISSION PARITAIRE POUR LES TAXIS",
+    "SOUS-COMMISSION PARITAIRE POUR LE TRANSPORT ROUTIER ET LA LOGISTIQUE POUR COMPTE DE TIERS",
+    "SOUS-COMMISSION PARITAIRE POUR L'ASSISTANCE EN ESCALE DANS LES AEROPORTS",
+    "SOUS-COMMISSION PARITAIRE POUR LE DEMENAGEMENT"]],
+    "COMMISSION PARITAIRE AUXILIAIRE POUR EMPLOYES": [200, []],
+    "COMMISSION PARITAIRE POUR EMPLOYES DE L'INDUSTRIE CHIMIQUE": [207, []],
+    "COMMISSION PARITAIRE POUR EMPLOYES DES FABRICATIONS METALLIQUES": [209, []],
+    "COMMISSION PARITAIRE POUR LES EMPLOYES DE L'INDUSTRIE ALIMENTAIRE": [220, []],
+    "COMMISSION PARITAIRE POUR LES BANQUES": [310,
+    ["SOUS-COMMISSION PARITAIRE DE L'EMPLOI DANS LE SECTEUR BANCAIRE (ABROGEE DEPUIS LE 28/12/2001)"]],
+    "COMMISSION PARITAIRE DES GRANDES ENTREPRISES DE VENTE AU DETAIL": [311, []]
+}
 
 def main():
     page_bg_img = '''
@@ -18,25 +37,26 @@ def main():
     st.markdown("###")
     st.header("Legal document")
     # st.markdown(page_bg_img, unsafe_allow_html=True)
-    category_dict = {
-        "None": [],
-        "Crime": ["murder", "theft", "destruction"],
-        "Salary": ["starting", "increase", "commission"],
-        "Contract": ["start date", "end date", "modification date"]
-    }
+  
     categories = ()
     for category in category_dict:
         categories = categories + (category, )
     mode = st.selectbox("Select Category",categories)
     st.markdown("###")
-    if len(category_dict[mode]) > 0:
-        st.radio("Sub Category", category_dict[mode])
+    if len(category_dict[mode][1]) > 0:
+        st.radio("Sub Category", category_dict[mode][1])
+    else:
+        st.write(mode)
+
     st.markdown("###")
     date = st.date_input("Effective date", datetime.date(2021,3,4))
     st.write(date)
     st.markdown("###")
+
     with open('pipeline/model/clas.pkl', 'rb') as f:
         clas = pickle.load(f)
+
+    url = "https://public-search.emploi.belgique.be/website-download-service/joint-work-convention/"
     
     for row in clas.iterrows():
         start_date = row[1][6]
@@ -45,10 +65,15 @@ def main():
         valid_date(end_date)
         st.write(row[1][6])
         st.write(row[1][7])
+        
+        file_link = url + row[0].replace("-", "/", 1)
+
+        st.write("["+ row[0] + "]" + "(" + file_link + ")")
         break
     st.write(clas)
 
-    st.button("Analyse")
+    if st.button("Analyse"):
+        st.write("check out this [ link ]( https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
 
 def valid_date(start_date):
     st.write(start_date[0:10])
