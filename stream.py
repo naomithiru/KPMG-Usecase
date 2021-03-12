efa622e19126d9417c107cb5ff5794642eedb956
Davy Nimbona <davynimbona@gmail.com>
2021-03-12 13:20:32 +0100

    Last message

--- stream.py
+++ stream.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as stc
-import pandas as pd
 from datetime import date, datetime
 from dateutil import relativedelta
 import pickle
 import codecs
+import pathlib
 
 
 category_dict = {
@@ -20,29 +20,33 @@ category_dict = {
     140.04: "SOUS COMMISSION PARITAIRE POUR L'ASSISTANCE EN ESCALE DANS LES AEROPORTS",
     140.05: "SOUS COMMISSION PARITAIRE POUR LE DEMENAGEMENT",
     200: "COMMISSION PARITAIRE AUXILIAIRE POUR EMPLOYES",
-    207: "COMMISSION PARITAIRE POUR EMPLOYES DE L'INDUSTRIE CHIMIQUE",  
-    209: "COMMISSION PARITAIRE POUR EMPLOYES DES FABRICATIONS METALLIQUES", 
-    220: "COMMISSION PARITAIRE POUR LES EMPLOYES DE L'INDUSTRIE ALIMENTAIRE", 
-    310: "COMMISSION PARITAIRE POUR LES BANQUES", 
+    207: "COMMISSION PARITAIRE POUR EMPLOYES DE L'INDUSTRIE CHIMIQUE",
+    209: "COMMISSION PARITAIRE POUR EMPLOYES DES FABRICATIONS METALLIQUES",
+    220: "COMMISSION PARITAIRE POUR LES EMPLOYES DE L'INDUSTRIE ALIMENTAIRE",
+    310: "COMMISSION PARITAIRE POUR LES BANQUES",
     310.01: "SOUS COMMISSION PARITAIRE DE L'EMPLOI DANS LE SECTEUR BANCAIRE (ABROGEE DEPUIS LE 28/12/2001)",
     311: "COMMISSION PARITAIRE DES GRANDES ENTREPRISES DE VENTE AU DETAIL"
 }
+
 
 def main():
     page_bg_img = '''
     <style>
         body {
-            background-image: url("https://images.unsplash.com/photo-1521587760476-6c12a4b040da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80");
+            background-image: url("https://pimmedia.egger.com/l/decor/U775_9/s/Detail/f/881x513/8803438034974");
             background-size: cover;
         }
     </style>
     '''
-
-    st.title("KPMG")
+    fname = pathlib.Path('pipeline/model/clas.pkl')
+    last_update = datetime.fromtimestamp(fname.stat().st_mtime)
+    update_html = "<h3>Last update : {}".format(str(last_update)[0:10])
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.markdown(update_html, unsafe_allow_html=True)
+    st.title("Partena CLA Monitor")
     st.markdown("###")
     st.header("Legal document")
-    # st.markdown(page_bg_img, unsafe_allow_html=True)
-  
+
     categories = ()
     for category in category_dict:
         category_name = str(category) + "- " + category_dict[category]
@@ -53,9 +57,10 @@ def main():
     keys = [sector.split("-")[0].lstrip() for sector in selection]
     st.markdown("###")
 
-    chosen_date = st.date_input("Effective date", date.today())
+    chosen_date = st.date_input("Choose date", date.today())
     effective_date = chosen_date + relativedelta.relativedelta(months=1, day=1)
-    st.write(effective_date)
+    date_html = "<p>The effective date:  {}</p>".format(effective_date)
+    st.markdown(date_html, unsafe_allow_html=True)
     st.markdown("###")
 
     keywords = [x.strip() for x in st.text_input("Add keywords or phrases (Separate words, by comma)").split(",")]
@@ -73,39 +78,42 @@ def main():
 def generate_links(date, sectors, keywords):
     with open('pipeline/model/clas.pkl', 'rb') as f:
         clas = pickle.load(f)
-    
-    st.write(clas)
 
     html_message = ""
     number = 1
 
     for row in clas.iterrows():
         check = 0
-        if str(row[1][6]) == "nan" and str(row[1][9]) == "nan":
+
+        if str(row[1][6]) == "nan" and str(row[1][10]) == "nan":
             pass
         elif str(row[1][6]) == "nan":
-            if row[1][9] <= date and row[1][1] in sectors and keyword_search(row[1][8], keywords):
+            if row[1][10] <= date and row[1][1] in sectors and keyword_search(row[1][8], keywords):
                 number += 1
                 check += 1
-        elif str(row[1][9]) == "nan":
+        elif str(row[1][10]) == "nan":
             if row[1][6] >= date and row[1][1] in sectors and keyword_search(row[1][8], keywords):
                 number += 1
                 check += 1
         else:
-            if row[1][9] <= date and row[1][6] >= date and row[1][1] in sectors and keyword_search(row[1][8], keywords):
+            if row[1][10] <= date and row[1][6] >= date and row[1][1] in sectors and keyword_search(row[1][8], keywords):
                 number += 1
                 check += 1
-        
+
         if check == 1:
             html_message += get_row_details(row, number)
-    get_html_page(html_message)
-    
+    if len(html_message) > 0:
+        get_html_page(html_message)
+    else:
+        html_no_result = "<p style=\"size=50px; color: red;\">No results found</p>"
+        st.markdown(html_no_result, unsafe_allow_html=True)
+
 
 def get_row_details(row, number):
-    deposit = str(row[1][0])
-    start_date = str(row[1][9])
-    end_date = str(row[1][6])
-    jcid = str(row[1][1])
+    publi_date = get_nice_date(date_format(row[1][4]))
+    start_date = get_nice_date(row[1][10])
+    end_date = get_nice_date(row[1][6])
+    scope = str(row[1][5])[0:20] + '...'
     url = "https://public-search.emploi.belgique.be/website-download-service/joint-work-convention/"
     file_link = url + row[0].replace("-", "/", 1)
     # column1 = "width: 260px;"
@@ -114,7 +122,16 @@ def get_row_details(row, number):
     # column4 = "width: 110px;"
     # column5 = "width: 222px;"
 
-    html_column = "<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td><a href=\"{}\">Download file</a></td></tr>".format(deposit, start_date, end_date, jcid, file_link)
+    html_column = "<tr>\
+                        <td>{}</td>\
+                        <td>{}</td>\
+                        <td>{}</td>\
+                        <td>{}</td>\
+                        <td>{}</td>\
+                        <td>{}</td>\
+                        <td>{}</td>\
+                        <td><a href=\"{}\">Download file</a></td>\
+                   </tr>".format(str(row[1][0]), str(row[1][2]), publi_date, scope, str(row[1][1]), start_date, end_date, file_link)
     # html_code = '<h4>'+ str(number) + ". " + str(deposit)+ '</h4>'
     # st.markdown(html_code, unsafe_allow_html=True)
     # hyperlink = "["+ row[0] + "]" + "(" + file_link + ")"
@@ -133,21 +150,27 @@ def keyword_search(text, keyword_list):
 
 
 def get_html_page(columns):
-    column1 = "width: 260px;padding-left: 40px;"
-    column2 = "width: 160px;"
-    column3 = "width: 245px;"
-    column4 = "width: 110px;text-align: right;"
-    column5 = "width: 222px;text-align: right;padding-right: 62px;"
+    column1 = "width: 100px;text-align: left;"
+    column2 = "width: 120px;text-align: center;"
+    column3 = "width: 120px;text-align: left;"
+    column4 = "width: 110px;text-align: center;"
+    column5 = "width: 60px; text-align: right"
+    column6 = "width: 120px;text-align: left"
+    column7 = "width: 120px;text-align: left"
+    column8 = "width: 100px;text-align: left"
 
     html_code = "<div>\
                     <div>\
                         <table>\
                             <thead>\
-                                <tr>\
+                                <tr style=\"background_color: black; font-family:courier,arial,helvetica; color: #fffdfc\">\
                                     <th style=\"{}\">Deposit Number</th>\
+                                    <th style=\"{}\">Title</th>\
+                                    <th style=\"{}\">Publication Date</th>\
+                                    <th style=\"{}\">Scope</th>\
+                                    <th style=\"{}\">JCID</th>\
                                     <th style=\"{}\">Start Date</th>\
                                     <th style=\"{}\">End Date</th>\
-                                    <th style=\"{}\">JCID</th>\
                                     <th style=\"{}\">Link to File</th>\
                                 </tr>\
                             </thead>\
@@ -156,22 +179,43 @@ def get_html_page(columns):
                             </tbody>\
                         </table>\
                     </div>\
-                </div>".format(column1, column2, column3, column4, column5, columns)
-    
+                </div>".format(column1, column2, column3, column4, column5, column6, column7, column8, columns)
+
     st.write(html_code, unsafe_allow_html=True)
 
 
 def get_from_html(htmf_file):
-     html_code = codecs.open(htmf_file, 'r')
-     page = html_code.read()
-     stc.html(page, width=700, height=500, scrolling=False)
+    html_code = codecs.open(htmf_file, 'r')
+    page = html_code.read()
+    stc.html(page, width=700, height=500, scrolling=False)
 
 
 def get_nice_date(raw_date):
+    if str(raw_date) == "nan":
+        return "Undefined"
     month_date = {
         1: "January",
-        2: ""
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
     }
+
+    return str(month_date[raw_date.month]) + " " + str(raw_date.year)
+
+
+def date_format(date_raw):
+    date_parts = date_raw[0:10]
+    new_date = datetime.strptime(date_parts, '%Y-%m-%d').date()
+
+    return new_date
 
 
 if __name__ == "__main__":
